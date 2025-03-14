13f4c1a3873ffe9c5542ff02010b79072eeb5d02
TigranEngineer <tigran.petrosyan2016@gmail.com>
2025-03-14 02:27:22 -0700

    Final fix for CLI

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,6 +41,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
 
 I2C_HandleTypeDef hi2c1;
 
@@ -56,6 +57,7 @@ static void MPU_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -174,6 +176,19 @@ static void Transmit_data(char *data)
   HAL_UART_Transmit(&huart1, (uint8_t *)data, strlen(data), 10);
 }
 
+#define MAGIC_NUMBER 10000
+
+static void ADC_Handler(void)
+{
+  uint32_t val = MAGIC_NUMBER;
+  HAL_ADC_Start(&hadc1);
+  while (val-- > 0) {
+    Transmit_data(To_Arr(HAL_ADC_GetValue(&hadc1)));
+    Transmit_data("\r\n");
+  }
+  HAL_ADC_Stop(&hadc1);
+}
+
 static void Transmit_answer(char *buff)
 {
   uint16_t size = strlen(buff) - 2;
@@ -194,11 +209,14 @@ static void Transmit_answer(char *buff)
     g_mod = OFF;
     Transmit_data("led turned off\r\n");
   } else if (!strcmp(buff, "led mode get\r\n")) {
-    char arr[42] = {0};
-    strcpy(arr, "led mode is ");
-    strcat(arr, To_Arr(g_freq));
-    strcat(arr, "\r\n");
-    Transmit_data(arr);
+    // char arr[42] = {0};
+    // strcpy(arr, "led mode is ");
+    // strcat(arr, To_Arr(g_freq));
+    // strcat(arr, "\r\n");
+    // Transmit_data(arr);
+    Transmit_data("led mode is ");
+    Transmit_data(To_Arr(g_freq));
+    Transmit_data("\r\n");
   } else if (!strncmp(buff, "led mode set ", strlen("led mode set "))) {
     uint32_t tmp = atoi(buff + strlen("led mode set "));
     if (tmp < 1 || tmp > 5000) {
@@ -211,8 +229,9 @@ static void Transmit_answer(char *buff)
   } else if (!strcmp(buff, "led mode reset\r\n")){
     g_freq = Default_Mod();
     Transmit_data("led mode has been reset successfully\r\n");
-  }
-  else {
+  } else if (!strcmp(buff, "adc read\r\n")) {
+    ADC_Handler();
+  } else {
     Transmit_data("command not found\r\n");
   }
 }
@@ -269,6 +288,7 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_USART1_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -327,6 +347,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_8;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
